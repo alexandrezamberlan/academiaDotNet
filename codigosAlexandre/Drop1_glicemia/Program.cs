11fa2dde3b548881333ab52161285ec789eff124
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2022-05-18 19:26:56 -0300

    drop1 o menu

---
+++ codigosAlexandre/Drop1_glicemia/Program.cs
@@ -0,0 +1,51 @@
+﻿// 1) Fazer um programa em VS que gere uma lista com n valores randômicos e inteiros para glicemia (entre 45 a 500). 
+// O progrma deve calcular média, valor mínimo, valor máximo e mediana.
+// Sugere-se criar um menu como:
+
+// MENU
+// 1 - Gerar lista
+// 2 - Exibir lista
+// 3 - Mostrar medidas centrais (média, valores min e max e mediana)
+// 4 - Sair
+// Opção: ____
+
+// Observação, toda vez que a opção 1 for acionada, o usuário deve definir quantos números serão gerados na lista
+
+// listaGlicemia é um objeto da classe List, logo tem atributos e métodos vindos da classe: Count; Add(); Contains()
+List<int> listaGlicemia = new List<int>();
+
+// opcao é um objeto da classe string, logo tem atributos e métodos vindos da classe: ToUpper(); Length; Split()
+string opcao;
+
+do {
+    Console.Clear();
+    Console.WriteLine("MENU");
+    Console.WriteLine("1 - Gerar lista");
+    Console.WriteLine("2 - Exibir lista");
+    Console.WriteLine("3 - Mostrar medidas centrais (média, valores min e max e mediana)");
+    Console.WriteLine("4 - Sair");
+    Console.Write("Opção: ");
+    opcao = Console.ReadLine();
+
+    switch  (opcao) {
+        case "1":
+            //gerar lista aleatoria
+            break;
+        case "2":
+            //exibir lista aleatoria
+            break;
+        case "3":
+            //mostrar medidas centrais
+            break;
+        case "4":
+            Console.WriteLine("Obrigado por usar o sistema");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+    Console.Write("Pressione algo para continuar!");
+    Console.ReadKey();
+} while (opcao != "4");
+
