5295f27429cc7536f4cc37039e2d7a19850f40d4
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2022-05-18 22:03:19 -0300

    drop4

--- codigosAlexandre/Drop4_emailArquivo/Program.cs
+++ codigosAlexandre/Drop4_emailArquivo/Program.cs
@@ -13,3 +13,99 @@
 // 3 - Mostrar dominíos de emails
 // 4 - Sair
 // Opção:____
+
+List<string> listaEmails = new List<string>();
+List<string> listaDominios = new List<string>();
+string opcao;
+
+do {
+    Console.Clear();
+    Console.WriteLine("MENU");
+    Console.WriteLine("1 - Abrir arquivo com emails e popular lista");
+    Console.WriteLine("2 - Exibir lista de emails");
+    Console.WriteLine("3 - Mostrar os domínios dos emails");
+    Console.WriteLine("4 - Sair");
+    Console.Write("Opção: ");
+    opcao = Console.ReadLine();
+
+    switch  (opcao) {
+        case "1":
+            //cadastrar email
+            Console.WriteLine("Abrindo arquivo de emails e populando a lista");
+            string email;
+
+            //abrir e ler os dados do arquivo, populando na lista
+            try //tratamento de exceções
+            {
+                String nomeArquivo;
+                Console.Write("Digite o nome do arquivo com os emails: ");
+                nomeArquivo = Console.ReadLine();
+                StreamReader leitor = new StreamReader(nomeArquivo); //abre o arquivo para leitura
+                //StreamReader leitor = new StreamReader(nomeArquivo, Encoding.UTF8); //abre o arquivo para leitura
+
+                do
+                {
+                    email = leitor.ReadLine();
+
+                    if (!listaEmails.Contains( email )) {
+                        listaEmails.Add( email );
+                        
+                        string[] emailSplit;
+                        string dominio;
+                        emailSplit = email.Split("@");
+                        dominio = emailSplit[1];
+                        if (!listaDominios.Contains(dominio)) {
+                            listaDominios.Add( dominio );
+                        }
+                        listaDominios.Sort();
+                    }
+
+                } while (!leitor.EndOfStream);
+
+                leitor.Close(); //fecha o objeto que representa o arquivo
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Erro ao ler o arquivo: " + e.Message);
+            }        
+            break;
+        case "2":
+            //exibir lista dos emails
+            Console.WriteLine("Exibindo a lista de emails");
+            if (listaEmails.Count == 0) {
+                Console.WriteLine("Lista Vazia!");
+            } else {
+                for (int i = 0; i < listaEmails.Count; i++)
+                {
+                    Console.WriteLine(listaEmails[i]);
+                }
+                // foreach (int i in listaEmails)
+                // {
+                //     Console.WriteLine(i);
+                // }
+            }
+            break;
+        case "3":
+            Console.WriteLine("Exibindo os domínios dos emails");
+            if (listaEmails.Count == 0) {
+                Console.WriteLine("Lista Vazia!");
+            } else {
+                //listaDominios.Sort(); - poderia ser aqui
+                foreach (string i in listaDominios)
+                {
+                    Console.WriteLine(i);
+                }                
+            }
+            break;
+        case "4":
+            Console.WriteLine("Obrigado por usar o sistema");
+            break;
+        default:
+            Console.WriteLine("Opção inválida!");
+            break;
+    }
+
+    Console.Write("Pressione algo para continuar!");
+    Console.ReadKey();
+} while (opcao != "4");
+
