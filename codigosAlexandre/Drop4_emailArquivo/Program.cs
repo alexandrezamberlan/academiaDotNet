4ec463a02ab332877b3f4cd34fc74bce3c3900a1
Alexandre Zamberlan <alexandre.o.zamberlan@gmail.com>
2022-05-18 21:37:08 -0300

    drop4

---
+++ codigosAlexandre/Drop4_emailArquivo/Program.cs
@@ -0,0 +1,15 @@
+﻿// 4) Fazer um programa em VS, como o programa de número 2, entretando os emails devem ser coletados de um arquivo
+// e populados em uma lista. Uma vez populados os emails na lista, o programa deve separar o nome de usuário (antes do @)
+// do domínio do email (depois do @). Ao final, o programa deve exibir os domínios utilizados no cadastro 
+// (não pode exibir os repetidos).
+
+// Observação: os domínio são, por exemplo, gmail.com; yahoo.com.br; ufn.edu.br; hotmail.com; entre outros.
+
+// Sugere-se usar um menu como:
+
+// MENU 
+// 1 - Abrir arquivo com emails e popular lista
+// 2 - Mostrar lista de emails
+// 3 - Mostrar dominíos de emails
+// 4 - Sair
+// Opção:____
