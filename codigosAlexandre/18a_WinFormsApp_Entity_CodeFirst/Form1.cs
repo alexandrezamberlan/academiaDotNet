b9350cde78e7b81de442ad4617529fef71c87be7
Alexandre Zamberlan <alexz@ufn.edu.br>
2022-07-05 17:03:25 -0300

    esqueleto para exercicio com entity - code firts

---
+++ codigosAlexandre/18a_WinFormsApp_Entity_CodeFirst/Form1.cs
@@ -0,0 +1,101 @@
+namespace _18a_WinFormsApp_Entity_CodeFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void limparCampos()
+        {
+            textBox_nomePaciente.Text = String.Empty;
+            textBox_email.Text = String.Empty;
+        }
+
+        private void carregarListView()
+        {
+            try
+            {
+                //listar contexto
+
+                listView_pacientes.Items.Clear();
+                int i = 0;
+                //repeticao na lista de pacientes do contexto
+                //while ()
+                //{
+                //    listView_pacientes.Items.Add(leitor["idPaciente"].ToString());
+                //    listView_pacientes.Items[i].SubItems.Add(leitor["nome"].ToString());
+                //    listView_pacientes.Items[i].SubItems.Add(leitor["email"].ToString());
+                //    i++;
+                //}               
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problemas de Conexão com o Banco " + ex.Message, "Alerta");
+            }
+        }
+
+        private void button_pesquisar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_editar_Click(object sender, EventArgs e)
+        {
+            panel_campos.Enabled = true;
+
+
+
+
+        }
+
+        private void button_deletar_Click(object sender, EventArgs e)
+        {
+
+            try
+            {                
+                int idPaciente = int.Parse(listView_pacientes.SelectedItems[0].Text);
+
+                //pesquisar no contexto
+                
+                this.limparCampos();
+                //recarregar ListView
+                this.carregarListView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Problemas de Conexão com o Banco " + ex.Message, "Alerta");
+            }
+
+
+
+        }
+
+        private void button_cadastrar_Click(object sender, EventArgs e)
+        {
+            panel_campos.Enabled = true;
+
+
+
+
+            
+        }
+
+        private void button_salvar_Click(object sender, EventArgs e)
+        {
+
+            //capturar valores das variáveis
+            
+            string nome = textBox_nomePaciente.Text;
+            string email = textBox_email.Text;
+
+
+
+
+
+            this.limparCampos();
+            panel_campos.Enabled = false;
+        }
+    }
+}
