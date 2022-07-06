2d0df834fc4f71eb01a3264a88f925651d3d7cae
Alexandre Zamberlan <alexz@ufn.edu.br>
2022-07-05 22:00:17 -0300

    exemplos entity code first

---
+++ codigosAlexandre/19_ConsoleApp_EntityFramework_CodeFirst/ClienteContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _19_ConsoleApp_EntityFramework_CodeFirst
+{
+    public class ClienteContext : DbContext
+    {
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
